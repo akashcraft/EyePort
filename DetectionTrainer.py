7aedd620d2a89e4d3f2da6cf9fec7e84436d0ef8
Akash Samanta <113077967+akashcraft@users.noreply.github.com>
2023-12-22 20:06:45 -0330

    Delete DetectionTrainer.py

--- DetectionTrainer.py
+++
@@ -1,8 +0,0 @@
-from imageai.Detection.Custom import DetectionModelTrainer
-
-trainer = DetectionModelTrainer()
-trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="Ocean")
-trainer.setTrainConfig(object_names_array=["iceberg","boat","ship"]
-                       , batch_size=5, num_experiments=500, train_from_pretrained_model="yolov3.pt")
-trainer.trainModel()
